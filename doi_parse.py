1b0df727d6f316584a8694e52f8836f81fb9846b
Keaton Armentrout <k.armentrout11@gmail.com>
2016-03-09 17:37:59 -0500

    added my files

---
+++ doi_parse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+from collections import Counter
+
+# Read in dois.txt and separate by commas
+f = open('dois.txt', 'r')
+dois = f.read().split(',')
+
+# Remove all "" elements
+dois = list(set(dois) - set(""))
+
+prefixes = []
+names = []
+
+for x in range(len(dois)):
+    if dois[x][2:5] != '10.':
+        continue
+    prefixes.append(dois[x][2:9])
+    names.append(dois[x][2:-1])
+
+# Count and return each unique prefix with the number of times it occurs
+p = Counter(prefixes)
+top = p.most_common(len(p))
+
+'''
+text = open('doi_to_provider.txt', 'a')
+for x in range(0, len(top)):
+    text.write(str(top[x]))
+    text.write('\n')
+'''
+
+print top
+
+# Create list of DOIs with each unique prefix represented once, in decreasing order of occurrence
+examples = []
+for x in range(len(p)):
+    pre = top[x][0]
+    for y in range(0,len(prefixes)):
+        if pre == prefixes[y]:
+            examples.append(names[y])
+            break
+
+print len(examples)
+print examples
+
