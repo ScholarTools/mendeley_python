6d2493541c39f0bec3d1808ff865b69b38cdbac1
JimHokanson <jim.hokanson@gmail.com>
2016-06-20 09:28:28 -0400

    bug fixes from merging

--- example_scripts/retrieve_user_library.py
+++ example_scripts/retrieve_user_library.py
@@ -2,6 +2,11 @@
 """
 """
 
+if __name__ == '__main__':
+    import sys
+    sys.path.append('..')
+
+#TODO: Try the import, if it fails, then append the path
 from mendeley import client_library
 
 #This call relies on information that has been stored in the user config file
